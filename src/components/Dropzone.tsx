11d777839508ea5c8f5150f53f74239308dd07c2
Daniel Jin <danieljin123@outlook.com>
2021-05-19 15:24:51 +0300

    removed unused lines

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -25,22 +25,22 @@ type Props = StateProps & DispatchProps & OwnProps;
 const Dropzone = (props: Props) => {
   const hiddenFileInput = React.useRef<HTMLInputElement>(null);
 
-  // function to get file size of inp file
-  const fileSize = (size: number) => {
-    if (size === 0) return '0 Bytes';
-    const k = 1024;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    const i = Math.floor(Math.log(size) / Math.log(k));
-    return parseFloat((size / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
-  };
+  // // function to get file size of inp file
+  // const fileSize = (size: number) => {
+  //   if (size === 0) return '0 Bytes';
+  //   const k = 1024;
+  //   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  //   const i = Math.floor(Math.log(size) / Math.log(k));
+  //   return parseFloat((size / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+  // };
 
-  // function to get filetype of file
-  const fileType = (fileName: string) => {
-    return (
-      fileName.substring(fileName.lastIndexOf('.') + 1, fileName.length) ||
-      fileName
-    );
-  };
+  // // function to get filetype of file
+  // const fileType = (fileName: string) => {
+  //   return (
+  //     fileName.substring(fileName.lastIndexOf('.') + 1, fileName.length) ||
+  //     fileName
+  //   );
+  // };
 
   const handleFileOpen = () => {
     hiddenFileInput.current?.click();
